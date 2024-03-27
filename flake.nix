a5114fa9fcd29081c06a859526b65a95a3ef9275
nokazn <41154684+nokazn@users.noreply.github.com>
2024-03-28 02:03:12 +0900

    chore: setup dev tools

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "nokazn/nokazn";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "github:edolstra/flake-compat";
+  };
+
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        shellHook = ''
+          pnpm install
+        '';
+
+        packages = with pkgs; [
+          nodejs
+          nodePackages.pnpm
+          biome
+          nixpkgs-fmt
+          dprint
+          yamlfmt
+          shfmt
+          treefmt
+        ];
+      };
+    });
+}
